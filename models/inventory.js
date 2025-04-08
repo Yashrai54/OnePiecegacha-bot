0e5266b1193c76a79bae120d270308527a02d427
Yash <starrai843@gmail.com>
2025-04-08 09:44:10 +0530

    mongoose error fix

--- models/inventory.js
+++ models/inventory.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
+const fruitSchema = new mongoose.Schema({
+  fruit: String,
+  user: String,
+  type: String,
+  image: String,
+  rarity: String
+});
+
 const inventorySchema = new mongoose.Schema({
-  userId: String,
-  fruits: [
-    {
-      fruit: String,
-      rarity: String,
-      type: String,
-      user: String,
-      image: String
-    }
-  ]
+  userId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  username: String,
+  fruits: [fruitSchema] // ✅ array of objects
 });
 
 module.exports = mongoose.model('Inventory', inventorySchema);
